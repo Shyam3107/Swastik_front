5bfb4de717223800132c7b6f93ee9ec2f860df4f
Shyam_Singhania <shyamsinghania4@gmail.com>
2022-01-30 20:02:38 +0530

    add github action for firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { Suspense, lazy } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import axios from "axios";
-import { connect } from "react-redux";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { Suspense, lazy } from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import axios from "axios"
+import { connect } from "react-redux"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
-import { backendURL } from "./APIs/APIs";
-import BackDropLoader from "./components/CustomComponents/BackDropLoader/BackDropLoader";
+import { backendURL } from "./APIs/APIs"
+import BackDropLoader from "./components/CustomComponents/BackDropLoader/BackDropLoader"
 
-const Navigation = lazy(() => import("./components/Navigation/Navigation"));
-const Login = lazy(() => import("./components/Login/Login"));
+const Navigation = lazy(() => import("./components/Navigation/Navigation"))
+const Login = lazy(() => import("./components/Login/Login"))
 
 axios.interceptors.request.use(
   (config) => {
     if (sessionStorage.getItem("token")) {
-      config.headers.Authorization = `Bearer ${sessionStorage.getItem(
-        "token"
-      )}`;
+      config.headers.Authorization = `Bearer ${sessionStorage.getItem("token")}`
     }
-    config.url = backendURL + config.url;
-    return config;
+    config.url = backendURL + config.url
+    return config
   },
   (error) => {
     // Do something with request error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 function App(props) {
-  const loggedIn = props.user.loggedIn;
+  const loggedIn = props.user.loggedIn
+
+  console.log("App started")
 
   return (
     <div className="App">
@@ -40,13 +40,13 @@ function App(props) {
         </Router>
       </Suspense>
     </div>
-  );
+  )
 }
 
 const mapStateToProps = (state) => {
   return {
     user: state.user,
-  };
-};
+  }
+}
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
