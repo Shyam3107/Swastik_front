068414b811d529d6a188f9860d296e4702d91d59
Shyam Pat Singhania <shyamsinghania4@gmail.com>
2023-11-19 00:48:26 +0530

    Try new xls read package

--- src/utils/convertCSVtoJSON.js
+++ src/utils/convertCSVtoJSON.js
@@ -1,4 +1,5 @@
 import Papa from "papaparse"
+//import * as XLSX from 'xlsx'
 import readXlsxFile from "read-excel-file"
 
 import toastMessage from "../components/CustomComponents/ToastMessage/toastMessage"
@@ -10,8 +11,10 @@ export const arrayToObj = (data) => {
   let arr = []
   let header = data[0]
   if (!header) header = []
+  // Values start from 2nd row
   for (let i = 1; i < data.length; i++) {
     let obj = {}
+    // Empty arrary [[]]
     if (data[i].length === 1) continue
     for (let j = 0; j < header.length; j++) {
       obj[header[j]] = data[i][j]
@@ -23,6 +26,7 @@ export const arrayToObj = (data) => {
 
 const convertCSVToJson = async (file, options) => {
   const extension = file.name.substr(file.name.length - 3, 3)
+  console.log(file)
 
   // Parse CSV File in array of Objects
   if (extension === "csv") {
@@ -36,8 +40,33 @@ const convertCSVToJson = async (file, options) => {
     })
   } else {
     // Parse Excel File in Array of objects
+    // var name = file.name;
+    // const reader = new FileReader();
+    // reader.onload = (evt) => { // evt = on_file_select event
+    //   /* Parse data */
+    //   const bstr = evt.target.result;
+    //   const wb = XLSX.read(bstr, { type: 'binary' });
+    //   /* Get first worksheet */
+    //   const wsname = wb.SheetNames[0];
+    //   const ws = wb.Sheets[wsname];
+    //   /* Convert array of arrays */
+    //   const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+    //   /* Update state */
+    //   console.log("Data>>>" + data);
+
+    //   // This is correct , choose this
+    //   const dataParse = XLSX.utils.sheet_to_json(ws, {header:1});
+    //   console.log("Data Parse>>>", dataParse)
+    // };
+    // reader.readAsBinaryString(file);
+
+
     readXlsxFile(file).then((data) => {
+      console.log("Excel data:", data)
       makeRequest({ ...options, payload: { data: arrayToObj(data) } })
+    }).catch((err) => {
+      console.log("Error :", err)
+      return toastMessage("Failed to read the File", error)
     })
   }
   return []
